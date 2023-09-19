e67a28cbb94ec0b3fb93b452d56efff02597a0d6
KevinChina <137712826@qq.com>
2023-09-20 00:04:40 +0800

    Update clip_interrogator.py
    不要从Hugging Face下载文件，直接使用本地已经下载好的

--- clip_interrogator/clip_interrogator.py
+++ clip_interrogator/clip_interrogator.py
@@ -46,7 +46,7 @@ class Config:
 
     # interrogator settings
     cache_path: str = 'cache'   # path to store cached text embeddings
-    download_cache: bool = True # when true, cached embeds are downloaded from huggingface
+    download_cache: bool = False # when true, cached embeds are downloaded from huggingface
     chunk_size: int = 2048      # batch size for CLIP, use smaller for lower VRAM
     data_path: str = os.path.join(os.path.dirname(__file__), 'data')
     device: str = ("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
